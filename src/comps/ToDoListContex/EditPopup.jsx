75c131c95fd62accd98665f652dbf5fcf4fe964b
Orel bukris <orelbukris7777@gmail.com>
2024-07-04 19:41:40 +0300

    react

--- src/comps/ToDoListContex/EditPopup.jsx
+++ src/comps/ToDoListContex/EditPopup.jsx
@@ -1,32 +1,38 @@
-import {useContext, useEffect, useState} from 'react';
-import {TaskContext} from './Context/TaskContext'
+import { useContext, useEffect, useState } from 'react';
+import { TaskContext } from './Context/TaskContext';
 
 export default function EditPopup() {
 
-  const {hideEditPopup,currentEdit,editTask} = useContext(TaskContext)
-  const [inpVal,setInpVal]=useState("");
-  useEffect(()=>{
-    setInpVal(currentEdit.name)
-  },[currentEdit])
+  const { hideEditPopup, currentEdit, editTask } = useContext(TaskContext);
+  const [nameVal, setNameVal] = useState("");
+  const [timeVal, setTimeVal] = useState("");
 
-  const onSub = (e)=>{
-    e.preventDefault()
-    editTask(currentEdit.id,{name:inpVal,id:currentEdit.id});
+  useEffect(() => {
+    setNameVal(currentEdit.name);
+    setTimeVal(currentEdit.time);
+  }, [currentEdit]);
+
+  const onSub = (e) => {
+    e.preventDefault();
+    editTask(currentEdit.id, { name: nameVal, time: timeVal, id: currentEdit.id });
     hideEditPopup();
-  }
+  };
+
   return (
     <div className='popup_edit'>
       <div className='inside_popup'>
         <form onSubmit={onSub}>
           <h2>Edit task</h2>
           <label>Name:</label>
-          <input value={inpVal} onChange={(e)=>setInpVal(e.currentTarget.value)} type="text"  className='form-control'/>
+          <input value={nameVal} onChange={(e) => setNameVal(e.currentTarget.value)} type="text" className='form-control' />
+          <label>Time:</label>
+          <input value={timeVal} onChange={(e) => setTimeVal(e.currentTarget.value)} type="time" className='form-control' />
           <button className='btn btn-success mt-3 me-2'>Update</button>
-          <button onClick={()=>{
+          <button onClick={() => {
             hideEditPopup();
           }} type="button" className='btn btn-danger mt-3 '>Cancel</button>
         </form>
       </div>
     </div>
-  )
+  );
 }
